770126a4511d08d1286668c7535be3a2889c3e2e
purva pawar <purvapramodpawar712@gmail.com>
2025-01-20 22:08:29 +0530

    abc

--- firstprogram.java
+++ firstprogram.java
@@ -1,5 +1,17 @@
-class firstprogram{
+class alphabetpattern {
     public static void main(String[] args) {
-        System.out.println("hello purva");
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++)
+             System.out.print(" ");
+        }
+        for (int j = 1; j <= 1; j++) {
+            System.out.print((char) ('A' + j - 1));
+        }
+        for (int j = i - 1; j >= 1; j--) {
+            System.out.print((char) ('A' + j - 1));
+        }
+        System.out.println();
     }
+
 }
--- pattern.java
+++ pattern.java
@@ -1,81 +1,91 @@
 import java.util.Scanner;
 
-public class pattern {
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
+class pattern {
+  public static void rightAscTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++)
+        System.out.print("*");
+      System.out.println();
+    }
+  }
 
-        System.out.println("1.right asc triangle");
-        System.out.println("2.leftasc triangle");
-        System.out.println("3.leftdesc triangle");
-        System.out.println("4.left desc triangle");
-        System.out.println("5.central diamond");
-        int a=scan.nextInt();
-        System.out.println("Enter number of rows in pattern");
-        int n = scan.nextInt();
-         switch(a)
-          {
-            case 1 :{ 
-              for(int i=1;i<=n;i++)
-            {
-              for(int j=1;j<i;j++)
-              System.out.print("*");
-              System.out.println();
-            }
-            break;
-          }
-            
+  public static void leftAscTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j < n - i; j++)
+        System.out.print(" ");
 
-      case 2:{ for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=n-i;j++)
-            System.out.print(" ");
+      for (int k = 1; k <= i; k++)
+        System.out.print("*");
+      System.out.println();
+    }
 
-            for(int k=1;k<=i;k++)
-            System.out.print("*");
-            System.out.println();
-        }
-        break;
+  }
+
+  public static void leftDescTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i - 1; j++)
+        System.out.print(" ");
+
+      for (int k = 1; k <= n + 1 - i; k++)
+        System.out.print("*");
+      System.out.println();
+    }
+  }
+
+  public static void rightDescTriangle(int n) {
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= n + 1 - i; j++)
+        System.out.print("*");
+      System.out.println();
+    }
+  }
+
+  public static void centralDiamond(int n) {
+    for (int i = 1; i <= n; i++) {
+      if (i <= n / 2) {
+        for (int j = 1; j <= n - i; j++)
+          System.out.print(" ");
+
+        for (int k = 1; k <= i; k++)
+          System.out.print("*");
+      } else {
+        for (int j = 1; j <= i - 1; j++)
+          System.out.print(" ");
+
+        for (int k = 1; k <= n + 1 - i; k++)
+          System.out.print("*");
       }
+      System.out.println();
+    }
+    System.out.println();
+  }
 
-      case 3 :{ for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=i-1;j++)
-            System.out.print(" ");
+  public static void main(String[] args) {
+    System.out.println("1.Right Asc Tiangle");
+    System.out.println("2.left Asc Triangle");
+    System.out.println("3.Left Asc Triangle");
+    System.out.println("4.Right Asc Triangle");
+    System.out.println("5.Central Diamond");
+    Scanner scan = new Scanner(System.in);
+    int choice = scan.nextInt();
+    switch (choice) {
 
-            for(int k=1;k<=2*n-1;k++)
-            System.out.print("*");
-            System.out.println();
-        }
+      case 1:
+        rightAscTriangle(5);
         break;
-      }
-      
-
-      case 4 :{for(int i=1;i<=n;i++)
-        {   for(int j=1;j<=i;j++)
-            System.out.print(" ");
-            System.out.println();
-        }
+      case 2:
+        leftAscTriangle(5);
         break;
-     }
-     
-
-     case 5 :{for(int i=1;i<=n;i++)
-        {if(i<=a/2)
-        {for(int j=1;j<=n-i;j++)
-            System.out.print(" ");
-
-            for(int k=1;k<=i;k++)
-            System.out.print("*");}
-        else
-        {for(int j=1;j<=i-1;j++)
-            System.out.print(" ");
-
-            for(int k=1;k<=n+1-1;k++)
-            System.out.print("*");}       
-        }System.out.println();
-            break;
-     }
+      case 3:
+        leftAscTriangle(5);
+        break;
+      case 4:
+        rightAscTriangle(5);
+        break;
+      case 5:
+        centralDiamond(6);
+        break;
 
     }
-}
+  }
 }
