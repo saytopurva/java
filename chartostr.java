5ec0b1cecf69c96c974ee761b8c17db1a7ea7b36
purva pawar <purvapramodpawar712@gmail.com>
2025-02-17 13:55:01 +0530

    p

--- chartostr.java
+++ chartostr.java
@@ -1,3 +1,17 @@
 public class chartostr {
-    
+    public static void main(String[] args) {
+        char ch = 'A';
+
+        String str1 = String.valueOf(ch);
+        System.out.println("Using String.valueOf(): " + str1);
+
+        String str2 = Character.toString(ch);
+        System.out.println("Using Character.toString(): " + str2);
+
+        String str3 = ch + "";
+        System.out.println("Using concatenation: " + str3);
+
+        String str4 = new String(new char[] { ch });
+        System.out.println("Using character array: " + str4);
+    }
 }
