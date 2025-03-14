48ae0ca0a32901ae6354dec4a927c039500a9913
purva pawar <purvapramodpawar712@gmail.com>
2025-03-14 17:39:18 +0530

    .

--- numericalinput.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class numericalinput {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("enter the alphabet string::");
-        String input = scanner.nextLine();
-    }
-}
